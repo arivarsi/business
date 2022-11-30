43f99828d4b587495423faa91c3af728de5c6136
everswick <everdamvarsi@gmail.com>
2022-11-30 22:36:24 +0200

    Initial

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+   
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>
+    "Mukoma Varsi has been with us all the way. We have established the brand, Digital rights through their guidance. . The amazing support & service. I couldn’t ask for any better. Thank you!""You guys are legendary! You guys are great and having amazing support & service.
+Mukoma Varsi helped us get Tasvika Sifikile onto the market. Thank you!"
+Mpumelelo Moyo
+Founder
+Ephraim Garwe
+Mukoma Varsi helped boost direct purchases of my Books from my site. My bookings have gone up 300 %. It has been a pleasure working with them so far.
+    </p>
+
+</article>
+
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+    "Mukoma Varsi has been with us all the way. We have established the brand, Digital rights through their guidance. . The amazing support & service. I couldn’t ask for any better. Thank you!""You guys are legendary! You guys are great and having amazing support & service.
+Mukoma Varsi helped us get Tasvika Sifikile onto the market. Thank you!"
+Mpumelelo Moyo
+Founder
+Ephraim Garwe
+Mukoma Varsi helped boost direct purchases of my Books from my site. My bookings have gone up 300 %. It has been a pleasure working with them so far.
+    </p>
+
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+    "Mukoma Varsi has been with us all the way. We have established the brand, Digital rights through their guidance. . The amazing support & service. I couldn’t ask for any better. Thank you!""You guys are legendary! You guys are great and having amazing support & service.
+Mukoma Varsi helped us get Tasvika Sifikile onto the market. Thank you!"
+Mpumelelo Moyo
+Founder
+Ephraim Garwe
+Mukoma Varsi helped boost direct purchases of my Books from my site. My bookings have gone up 300 %. It has been a pleasure working with them so far.
+    </p>
+
+</article>
+
+
+</body>
